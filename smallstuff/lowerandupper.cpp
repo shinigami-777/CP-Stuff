3bfd8035bf7d4abfb14fc0f326c2ff8397cb22a4
Tamaghna Chattopadhyay <139495942+shinigami-777@users.noreply.github.com>
2024-03-23 13:39:44 +0530

    binary search added

--- smallstuff/lowerandupper.cpp
+++ smallstuff/lowerandupper.cpp
@@ -6,3 +6,12 @@ cout << b-a << "\n";
 //using equal_range the code becomes shorter
 auto r = equal_range(array, array+n, x);
 cout << r.second-r.first << "\n";
+
+//binary search for finding the first value from where the function ok(x) starts changing
+//eg- false flase false true true
+//the ans in this eg is 3
+int x = -1;
+for (int b = z; b >= 1; b /= 2) {
+while (!ok(x+b)) x += b;
+}
+int k = x+1;
